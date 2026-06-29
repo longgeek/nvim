e684277e616014225e848f6fb64cecd76f337e11
longgeek <longgeek@gmail.com>
2026-06-29 15:05:31 +0800

    markdown: 加 render-markdown.nvim, buffer 内实时渲染 (无浏览器, <leader>mr 开关)
    当前字体非 Nerd Font, 图标用纯文本/unicode 代替 (标题/列表/勾选框/分隔线); 浏览器预览 <leader>mp 保留共存。

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,7 +7,27 @@ return {
     -- 用 :命令式 build: lazy 会先加载插件再执行该命令, 避开 build 时 autoload 未加载的 E117
     build = ":call mkdp#util#install()",
     keys = {
-      { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", ft = "markdown", desc = "Markdown 预览开关" },
+      { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", ft = "markdown", desc = "Markdown 浏览器预览开关" },
+    },
+  },
+
+  -- buffer 内实时渲染 markdown (无浏览器); 当前字体非 Nerd Font, 图标用纯文本/unicode 代替
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ft = { "markdown" },
+    opts = {
+      sign = { enabled = false },
+      heading = { icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " } },
+      bullet = { icons = { "•", "◦", "▪", "▫" } },
+      checkbox = {
+        unchecked = { icon = "[ ] " },
+        checked = { icon = "[x] " },
+      },
+      dash = { icon = "─" },
+    },
+    keys = {
+      { "<leader>mr", "<cmd>RenderMarkdown toggle<CR>", ft = "markdown", desc = "Markdown buffer 内渲染开关" },
     },
   },
 }
